3872f86fa96ee998ee339c2015534312e041f67b
ashmina <raiashmina404@gmail.com>
2023-07-01 16:02:52 +0545

    audio updated

--- js/client.js
+++ js/client.js
@@ -3,6 +3,11 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+var audio = new Audio('ting.mp4');
+if(position == 'left'){
+  audio.play();
+}
+
 
 const appendMessage = (message, position) => {
   const messageElement = document.createElement('div');
