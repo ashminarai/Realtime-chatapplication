4580dcc150faba347fb8796d3522585e018a5f7d
ashmina <raiashmina404@gmail.com>
2023-07-01 15:00:20 +0545

    updated

--- js/client.js
+++ js/client.js
@@ -1,27 +1,32 @@
-// // const { Namespace } = require("socket.io");
-
-// javascript file 
 const socket = io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp')
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
 
-// jati bela ni message aauchha tyo kaa rakhne ta yo container vanne div ko part ma
-const messageContainer = document.querySelector(".container")
+const appendMessage = (message, position) => {
+  const messageElement = document.createElement('div');
+  messageElement.innerText = message;
+  messageElement.classList.add('message', position);
+  messageContainer.append(messageElement);
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+};
 
-// append function
-const append = (message, position)=>{
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
-    messageContainer.append(messageElement);
-}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`, 'right');
+  socket.emit('send', message);
+  messageInput.value = '';
+});
 
-const name = prompt("Enter your name to join");
-socket.emit('new-user-joined', name);
+const names = prompt("Enter your name to join");
+socket.emit('new-user-joined', names);
 
+socket.on('user-joined', name => {
+  appendMessage(`${name} joined the chat`, 'right');
+});
 
-socket.on('user-joined', name =>{
-    append(`${name} joined the chat`, 'right')
-})
+socket.on('receive', data => {
+  appendMessage(`${data.name}: ${data.message}`, 'left');
+});
